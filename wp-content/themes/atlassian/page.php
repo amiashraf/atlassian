14372f5a1a1f63a68263fea8370ea65bf44b7c66
Ashraf Zaman <amiashraf@gmail.com>
2014-12-20 16:14:00 +0600

    Modify page.php

--- wp-content/themes/atlassian/page.php
+++ wp-content/themes/atlassian/page.php
@@ -13,36 +13,38 @@
 
 get_header(); ?>
 
-<div id="main-content" class="main-content">
+<main id="main" class="section">
+    <div class="container">
+        <div class="row">
+        <?php
+            if ( is_front_page() && atlassian_has_featured_posts() ) {
+                // Include the featured content template.
+                get_template_part( 'featured-content' );
+            }
+        ?>
+            <div id="primary" class="content-area col-xs-12 col-sm-12 col-md-9">
+                <div id="content" class="site-content" role="main">
 
+                    <?php
+                        // Start the Loop.
+                        while ( have_posts() ) : the_post();
+
+                            // Include the page content template.
+                            get_template_part( 'content', 'page' );
+
+                            // If comments are open or we have at least one comment, load up the comment template.
+                            /*if ( comments_open() || get_comments_number() ) {
+                                comments_template();
+                            }*/
+                        endwhile;
+                    ?>
+                </div><!-- #content -->
+            </div><!-- #primary -->
+        <?php
+        get_sidebar();
+        ?>
+        </div>
+    </div>
+</main>
 <?php
-	if ( is_front_page() && atlassian_has_featured_posts() ) {
-		// Include the featured content template.
-		get_template_part( 'featured-content' );
-	}
-?>
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-
-					// Include the page content template.
-					get_template_part( 'content', 'page' );
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
-				endwhile;
-			?>
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
-	<?php get_sidebar( 'content' ); ?>
-</div><!-- #main-content -->
-
-<?php
-get_sidebar();
 get_footer();
